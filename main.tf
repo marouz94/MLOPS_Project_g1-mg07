c2d3bfba236860587215518ee454e5ed744b9919
marouz94 <bemmoussatmarwan@gmail.com>
2025-12-28 17:08:23 +0100

    Refactor terraform main.tf structure
    Reorganize terraform configuration and backend settings.

--- main.tf
+++ main.tf
@@ -1,18 +1,20 @@
 terraform {
-required_version = ">= 1.5.0"
-required_providers {
-   aws = {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
-   }
-}
-# Configuration du backend S3 pour stocker le state
-backend "s3" {
-   bucket  = "infrastats-g1mg07"
-   key     = "g1mg07.tfstate"
-   region  = "eu-west-3"
-   encrypt = true
-} }
+    }
+  }
+
+  # Configuration du backend S3 pour stocker le state
+  backend "s3" {
+    bucket  = "infrastats-g1mg07"
+    key     = "g1mg07.tfstate"
+    region  = "eu-west-3"
+    encrypt = true
+  }
+} 
 
 # Bloc Provider indispensable
 provider "aws" {
