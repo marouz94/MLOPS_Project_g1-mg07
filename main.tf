c81b3c3ba9e443e1b755977c6b04e2f69cba3e39
marouz94 <bemmoussatmarwan@gmail.com>
2025-12-28 01:33:50 +0100

    Configure AWS App Runner service and IAM role
    Added AWS App Runner service and IAM role configuration.

--- main.tf
+++ main.tf
@@ -34,3 +34,51 @@ resource "aws_ecr_repository" "api_repo" {
 output "ecr_repository_url" {
   value = aws_ecr_repository.api_repo.repository_url
 }
+
+# Le Service App Runner
+resource "aws_apprunner_service" "sales_api_service" {
+  service_name = "apprunner-g1mg07" 
+
+  source_configuration {
+    authentication_configuration {
+      access_role_arn = aws_iam_role.apprunner_role_g1mg07.arn
+    }
+    image_repository {
+      image_identifier      = "${aws_ecr_repository.api_repo.repository_url}:latest"
+      image_repository_type = "ECR"
+      image_configuration {
+        port = "8000"
+      }
+    }
+    auto_deployments_enabled = true
+  }
+}
+
+#  Le Rôle IAM spécifique au groupe
+resource "aws_iam_role" "apprunner_role_g1mg07" {
+  name = "role-apprunner-g1mg07"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "build.apprunner.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+#  L'attachement des droits de lecture ECR
+resource "aws_iam_role_policy_attachment" "apprunner_policy_g1mg07" {
+  role       = aws_iam_role.apprunner_role_g1mg07.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
+
+# Output pour ton rapport final
+output "app_runner_url" {
+  value = aws_apprunner_service.sales_api_service.service_url
+}
